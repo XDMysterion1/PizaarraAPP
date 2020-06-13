59972dcf05b5a05983287fcc6c0a86a8f43fa25b
Denisse-vargasH <rinadenissevargas@gmail.com>
2020-06-13 02:41:34 -0400

    CrearbarraHerramienta

---
+++ src/pizarraapp/BarraHerramienta.java
@@ -0,0 +1,31 @@
+
+package pizarraapp;
+
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class BarraHerramienta {
+    	private JToolBar 	  barraHerramientBar;
+	private JButton 	  lapiz;
+	private JButton		  linea;
+	private JButton 	  rectangulo;
+	private JButton 	  circulo;
+	private JButton 	  texto;
+	private JButton 	  borrador;
+	private JButton 	  llenar;
+	private JButton 	  deshacer;
+	private JButton 	  rehacer;
+	private JButton 	  limpiar;
+	private Dimension         nuevaDimencion = new Dimension(700,500);
+	private JButton 	  guardar;
+	private JButton 	  abrir;
+	private JButton 	  nuevoArchivo;
+	private DibujarMarco      marco;  
+        
+        public BarraHerramienta(DibujarMarco marco) {
+
+
+	}
+    
+}
